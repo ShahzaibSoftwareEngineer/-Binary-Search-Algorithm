8284b3fc96298b2b27ba25b8790a872dd2f59737
ShahzaibSoftwareEngineer <infoshahzaibmunir@gmail.com>
2025-03-26 13:09:38 -0700

    Create Binary.py

---
+++ Binary.py
@@ -0,0 +1,13 @@
+def program32():
+    def binary_search(arr, target):
+        low, high = 0, len(arr)-1
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+    print("Found at index:", binary_search([1, 2, 3, 4, 5], 3))  # 2
